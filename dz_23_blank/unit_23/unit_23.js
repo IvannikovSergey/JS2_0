4956504aa5e72975497269f5d35048a84b87646f
Sergey <nagoymail@gmail.com>
2022-01-06 21:12:29 +0500

    юнит 23 доделан

--- dz_23_blank/unit_23/unit_23.js
+++ dz_23_blank/unit_23/unit_23.js
@@ -1,4 +1,3 @@
-
 // Task 1 ============================================
 /* Создайте функцию t1 которая записывает  в LS  ключ 5 со значением 11. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-1. */
 
@@ -10,7 +9,8 @@ document.querySelector('.b-1').onclick = t1; // ваше событие здес
 
 // Task 2 ============================================
 /* Создайте функцию t2 которая записывает  в LS  массив a2 = [7,6,5]. Ключ a2. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-2. */
-a2 = [7, 6, 5,];
+a2 = [7, 6, 5, ];
+
 function t2() {
     localStorage.setItem('a2', JSON.stringify(a2));
 }
@@ -22,6 +22,7 @@ document.querySelector('.b-2').onclick = t2; // ваше событие здес
 // Task 3 ============================================
 /*  При нажатии кнопки t3 выведите из LS сохранненный массив a2. Выведите в out-3 в формате ключ пробел значение перенос строки.  */
 let out = '';
+
 function t3() {
     let a3 = localStorage.getItem('a2');
     a3 = JSON.parse(a3);
@@ -29,7 +30,6 @@ function t3() {
         out += i + ' ' + a3[i] + "<br>";
     }
     document.querySelector('.out-3').innerHTML = out;
-    console.log(a3);
 }
 
 document.querySelector('.b-3').onclick = t3; // ваше событие здесь!!!
@@ -37,7 +37,11 @@ document.querySelector('.b-3').onclick = t3; // ваше событие здес
 
 // Task 4 ============================================
 /*  Создайте функцию t4 которая записывает  в LS  массив a4 = {hello: world, hi:mahai}. Ключ a4. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-4.*/
-a4 = { hello: 'world', hi: 'mahai' };
+a4 = {
+    hello: 'world',
+    hi: 'mahai'
+};
+
 function t4() {
     localStorage.setItem('a4', JSON.stringify(a4));
 }
@@ -47,6 +51,7 @@ document.querySelector('.b-4').onclick = t4; // ваше событие здес
 // Task 5 ============================================
 /*   При нажатии кнопки b-5 выведите из LS сохранненный массив a4. Выведите в out-5 в формате ключ пробел значение перенос строки. */
 let out5 = '';
+
 function t5() {
     let a5 = localStorage.getItem('a4');
     a5 = JSON.parse(a4);
@@ -54,7 +59,6 @@ function t5() {
         out5 += i + ' ' + a5[i];
     }
     document.querySelector('.out-5').innerHTML = out5;
-    console.log(a5);
 }
 
 document.querySelector('.b-5').onclick = t5; // ваше событие здесь!!!
@@ -63,28 +67,35 @@ document.querySelector('.b-5').onclick = t5; // ваше событие здес
 /*  Создайте функцию t6 которая очищает весь LS. Запуск по кнопке b-6*/
 
 function t6() {
-
+    localStorage.clear();
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-6').onclick = t6; // ваше событие здесь!!!
 
 
 // Task 7 ============================================
 /*  Создайте input i-7 куда пользователь может вводить числа и строки. Создайте массив a7. Когда пользователь нажимает кнопку b-7 число должно добавляться в массив. Массив должен сохраняться в LS с ключем a7.*/
+let a7 = [];
+let inp = document.querySelector('.i-7');
 
 function t7() {
-
+    if (inp.value != '') {
+        a7.push(inp.value);
+        localStorage.setItem('a7', JSON.stringify(a7));
+        inp.value = null;
+    }
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-7').onclick = t7; // ваше событие здесь!!!
 
 // Task 8 ============================================
 /*   Создайте функцию t8 при запуске которой из a7 удаляется последний элемент. После чего массив сохраняется в LS с ключем a7. Использовать массив из предыдущего задания. */
 
 function t8() {
-
+    let a = localStorage.getItem('a7');
+    let arr = JSON.parse(a);
+    arr.pop();
+    localStorage.setItem('a7', JSON.stringify(arr));
 }
 
-// ваше событие здесь!!!
-
-
+document.querySelector('.b-8').onclick = t8; // ваше событие здесь!!!
