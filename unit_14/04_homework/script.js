21b0b2e7f414e3067996a99f447969acefd6e559
Sergey <nagoymail@gmail.com>
2022-01-19 12:19:46 +0500

    доделан 14 юнит

--- unit_14/04_homework/script.js
+++ unit_14/04_homework/script.js
@@ -3,18 +3,39 @@ const param = {
     'appid': 'd2fee8cb7e682df6daf0164000130fdd'
 }
 
+const cities = {
+    2643743: "Лондон",
+    625144: "Минск",
+    1512569: 'Ташкент',
+    703448: 'Киев'
+}
+function addCity() {
+    let div = document.createElement('select');
+    div.classList.add('sel');
+    for (let k in cities) {
+        let option = document.createElement('option');
+        option.value = k;
+        option.text = cities[k];
+        div.append(option);
+    };
+    document.querySelector('.out').before(div);
+    document.querySelector('.sel').onchange = getWeather;
+    getWeather();
+
+}
+
 function getWeather() {
-    const cityId = document.querySelector('#city').value;
-    fetch(`${param.url}weather?id=${cityId}&units=metric&appid=${param.appid}`)
+    const cityId = document.querySelector('.sel').value;
+    fetch(`${param.url}weather?id=${cityId}&appid=${param.appid}`)
         .then(weather => {
             return weather.json();
+
         }).then(showWeather);
 }
 
 function showWeather(data) {
-    console.log(data);
-    document.querySelector('.out').innerHTML = data.temp;
+    document.querySelector('.out').innerHTML = `Температура: ${Math.round(data.main.temp - 273)}C&deg<br>
+    Влажность: ${data.main.humidity}%`;
 }
 
-getWeather();
-document.querySelector('#city').onchange = getWeather;
+addCity();
